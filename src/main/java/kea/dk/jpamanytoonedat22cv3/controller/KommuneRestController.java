e78a7e443c2744cc50fc87ea6c1a47b33e3e93ed
ArthurOS <arth3608@stud.kea.dk>
2023-09-15 14:51:19 +0200

    Opgave 1 Done

--- src/main/java/kea/dk/jpamanytoonedat22cv3/controller/KommuneRestController.java
+++ src/main/java/kea/dk/jpamanytoonedat22cv3/controller/KommuneRestController.java
@@ -7,13 +7,11 @@ import kea.dk.jpamanytoonedat22cv3.service.ApiServiceKommunerImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class KommuneRestController
 {
@@ -26,7 +24,7 @@ public class KommuneRestController
     List<Kommune> getKommuner(){
         return apiServiceKommuner.getKommuner();
     }
-    @DeleteMapping("/kommuneDelete/{regionId}")
+    @DeleteMapping("/kommune/{regionId}")
     public ResponseEntity<String> deleteKommuneByRegionId(@PathVariable String regionId) {
         try {
             kommuneRepository.deleteKommuneByRegionKode(regionId);
--- src/main/java/kea/dk/jpamanytoonedat22cv3/controller/RegionRestController.java
+++ src/main/java/kea/dk/jpamanytoonedat22cv3/controller/RegionRestController.java
@@ -9,15 +9,13 @@ import kea.dk.jpamanytoonedat22cv3.service.ApiServiceRegion;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin
 @RestController
 public class RegionRestController
 {
